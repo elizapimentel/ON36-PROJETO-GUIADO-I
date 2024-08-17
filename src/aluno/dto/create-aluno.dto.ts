ed997eefdc94c64df3b682c41c82a0c55b20d562
elizapimentel <elizapimentel@hotmail.com>
2024-08-17 17:05:52 -0300

    feat: add dto details

--- src/aluno/dto/create-aluno.dto.ts
+++ src/aluno/dto/create-aluno.dto.ts
@@ -1,1 +1,23 @@
-export class CreateAlunoDto {}
+import { IsEmail, IsNotEmpty, IsNumber, IsString, Max } from 'class-validator';
+
+const currentDate = new Date();
+const currentYear = currentDate.getFullYear();
+
+export class CreateAlunoDto {
+  @IsString()
+  nome: string;
+
+  @IsString()
+  endereco: string;
+
+  @IsString()
+  telefone: string;
+
+  @IsNotEmpty()
+  @IsEmail()
+  email: string;
+  
+  @IsNumber()
+  @Max(currentYear - 16)
+  anoNascimento: number;
+}
