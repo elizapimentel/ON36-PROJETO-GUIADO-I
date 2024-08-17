35ef15ad7333cbe8528d9775192a1ee40aa9d666
elizapimentel <elizapimentel@hotmail.com>
2024-08-17 17:06:13 -0300

    feat: add aluno entity

--- src/aluno/entities/aluno.entity.ts
+++ src/aluno/entities/aluno.entity.ts
@@ -1,1 +1,8 @@
-export class Aluno {}
+export class Aluno {
+    id: string;
+    cursos: string[];
+
+    constructor(public nome: string, public endereco: string, public telefone: string, public email: string) {
+        this.cursos = [];
+    }
+}
