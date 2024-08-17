65aaeef801c26a36750cf6eb245aeb301b4fcb71
elizapimentel <elizapimentel@hotmail.com>
2024-08-17 17:07:13 -0300

    refactor: add repo to providers

--- src/aluno/aluno.module.ts
+++ src/aluno/aluno.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AlunoService } from './aluno.service';
-import { AlunoController } from './aluno.controller';
+import { AlunoService } from '../aluno/services/aluno.service';
+import { AlunoController } from '../aluno/controllers/aluno.controller';
+import { AlunoRepository } from '../aluno/repositories/aluno.repository';
 
 @Module({
   controllers: [AlunoController],
-  providers: [AlunoService],
+  providers: [AlunoService, AlunoRepository],
 })
 export class AlunoModule {}
