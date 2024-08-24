b15702e792c7ea6bd53fe0e705ad70dcccd245e5
elizapimentel <elizapimentel@hotmail.com>
2024-08-23 21:41:34 -0300

    feat: add Curso

--- src/aluno/domain/aluno.ts
+++ src/aluno/domain/aluno.ts
@@ -1,9 +1,10 @@
+import { Curso } from '../../cursos/domain/curso';
 import { IPessoa } from '../../commons/interfaces/pessoa.interface';
 import { randomUUID as uuid } from 'crypto';
 
 export class Aluno implements IPessoa {
     id: string;
-    cursos: string[];
+    cursos: Curso[];
     
     constructor(
         public nome: string,
--- src/aluno/infra/adapters/inbound/controllers/aluno.controller.ts
+++ src/aluno/infra/adapters/inbound/controllers/aluno.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param } from '@nestjs/common';
 import { AlunoService } from '../../../../application/ports/input/aluno.service';
 import { CreateAlunoDto } from '../dto/create-aluno.dto';
 import { CreateAlunoCommand } from '../../../../application/commands/create-aluno-command';
@@ -25,18 +25,8 @@ export class AlunoController {
     return this.alunoService.getAll();
   }
 
-  // @Get(':id')
-  // findOne(@Param('id') id: string) {
-  //   return this.alunoService.findOne(+id);
-  // }
-
-  // @Patch(':id')
-  // update(@Param('id') id: string, @Body() updateAlunoDto: UpdateAlunoDto) {
-  //   return this.alunoService.update(+id, updateAlunoDto);
-  // }
-
-  // @Delete(':id')
-  // remove(@Param('id') id: string) {
-  //   return this.alunoService.remove(+id);
-  // }
+   @Post(':id/cursos/:cursoId')
+  async adicionarCurso(@Param('id') alunoId: string, @Param('cursoId') cursoId: string) {
+    return this.alunoService.adicionarCurso(alunoId, cursoId);
+  }
 }
